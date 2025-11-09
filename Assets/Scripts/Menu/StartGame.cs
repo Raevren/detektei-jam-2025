1731cb63d1666691af17f4edb4b95f08ffd4c85c
Pascal Helmerich <pascal@helmerich-software.de>
2025-11-09 14:27:44 +0100

    fix loadign issues in webgl

--- Assets/Scripts/Menu/StartGame.cs
+++ Assets/Scripts/Menu/StartGame.cs
@@ -1,10 +1,30 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.Localization.Settings;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Menu
 {
     public class StartGame : MonoBehaviour
     {
+        private Button button;
+        private void Start()
+        {
+            button = GetComponent<Button>();
+            button.enabled = false;
+            StartCoroutine(PreloadLocalization());
+        }
+        
+        IEnumerator PreloadLocalization()
+        {
+            yield return Addressables.InitializeAsync();
+            yield return LocalizationSettings.InitializationOperation;
+            
+            button.enabled = true;
+        }
+        
         public void StartGameScene()
         {
             SoundSystem.Instance.PlayGenericSfx(CommonSfx.Submit);
